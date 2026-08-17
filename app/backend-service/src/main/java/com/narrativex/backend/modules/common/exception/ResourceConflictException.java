36982cc3b2178610050f4a89b5e2f5bfcbf373e9
Nguyễn Ích Hưởng <131357173+huongni2201@users.noreply.github.com>
2026-08-18 02:40:33 +0700

    refactor: introduce common module shared kernel

---
+++ app/backend-service/src/main/java/com/narrativex/backend/modules/common/exception/ResourceConflictException.java
@@ -0,0 +1,7 @@
+package com.narrativex.backend.modules.common.exception;
+
+public class ResourceConflictException extends RuntimeException {
+    public ResourceConflictException(String message) {
+        super(message);
+    }
+}
