62e58f7c6335d1e1355eac25a4480a0a16a875db
Nguyễn Ích Hưởng <131357173+huongni2201@users.noreply.github.com>
2026-08-23 19:13:44 +0700

    refactor(storyboard): migrate ChapterAccess to UUID

--- app/backend-service/src/main/java/com/narrativex/backend/feature/storyboard/application/port/in/ChapterAccess.java
+++ app/backend-service/src/main/java/com/narrativex/backend/feature/storyboard/application/port/in/ChapterAccess.java
@@ -2,7 +2,8 @@ package com.narrativex.backend.feature.storyboard.application.port.in;
 
 import com.narrativex.backend.feature.storyboard.domain.aggregate.Chapter;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface ChapterAccess {
-  Optional<Chapter> findById(Long chapterId);
+  Optional<Chapter> findById(UUID chapterId);
 }
